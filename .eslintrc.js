32f1ba08db5d61e602c71f5ff848d83461d93967
Samrath <samrath.kumawat@navajna.co.in>
2020-11-09 15:14:02 +0530

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+// Copyright (C) 2018-2020 Intel Corporation
+//
+// SPDX-License-Identifier: MIT
+
+module.exports = {
+    env: {
+        node: false,
+        browser: true,
+        es6: true,
+        jquery: true,
+        qunit: true,
+    },
+    parserOptions: {
+        sourceType: 'script',
+    },
+    plugins: ['eslint-plugin-header'],
+    extends: ['eslint:recommended', 'prettier'],
+    rules: {
+        'header/header': [2, 'line', [{
+            pattern: ' {1}Copyright \\(C\\) (?:20\\d{2}-)?2020 Intel Corporation',
+            template: ' Copyright (C) 2020 Intel Corporation'
+        }, '', ' SPDX-License-Identifier: MIT']],
+    },
+};
